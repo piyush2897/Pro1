423108eebdd4ee36b4255f5c64f218885d784d80
piyush <piyushyadav2897@gmail.com>
2017-10-17 11:48:22 +0530

    Database Modified

--- Controllers/insert_another_site_details.js
+++ Controllers/insert_another_site_details.js
@@ -5,7 +5,22 @@ var url="mongodb://localhost:27017/pro1";
 module.exports = function(app)
 {
 	app.get('/insert_another_site',function(req,res){
-
+		var starterTodos =[
+			{
+				site: req.query.site_list,
+				username: req.query.username,
+				password: req.query.password,
+			}
+		];
+		mongoClient.connect(url,function(err,db){
+			if(err) throw err;
+			db.collection("user_sites_details").insertMany(starterTodos,
+				function(err,resu){
+					if(err) throw err;
+					res.render('user_details');
+				});
+			db.close();
+		});
 		
 	});
 }
--- Controllers/prints_sites.js
+++ Controllers/prints_sites.js
@@ -9,18 +9,18 @@ module.exports = function(app)
 		mongoClient.connect(url,function(err,db){
 			if(err) throw err;
 				var c;
-			var c=db.collection("user_site_details").count({},function(err,count)
+			var c=db.collection("user_sites_details").count({},function(err,count)
 			{
 				if(err) throw err;
 					c=count;
 			});
-			db.collection("user_site_details").find({}).toArray(function(err,resu)
+			db.collection("user_sites_details").find({}).toArray(function(err,resu)
 				{
 					if(err) throw err;
-					var view_screen='<html><body><div><table><tr><th>INDEX  </th><th>  UNIQUE ID  </th><th>  USERNAME  </th><th>  PASSWORD  </th></tr>';
+					var view_screen='<html><body><div><table><tr><th>INDEX  </th><th>  USERNAME  </th><th>  PASSWORD  </th><th>  SITE  </th></tr>';
 					for(i=0;i<c;i=i+1)
 					{
-					view_screen=  view_screen +'<tr><td>'+(i+1)+'</td><td>' + resu[i]._id +'</td><td>' + resu[i].username + '</td><td>'+resu[i].password+ '</td><td>'+resu[i].site_list+'</td></tr>';
+					view_screen=  view_screen +'<tr><td>'+(i+1)+'</td><td>' +resu[i].username + '</td><td>'+resu[i].password+ '</td><td>'+resu[i].site+'</td></tr>';
 					//console.log(resu[i].username+i);
 					//i=i+1;
 					}
